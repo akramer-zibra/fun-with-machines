59ab66148619f388f833e100d27dd6268d18efcd
Achim <achim@zibra.de>
2023-11-28 21:01:20 +0100

    Startet mit dem Beispielcode aus der ionic Dokumenation (React)

---
+++ src/main.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenu,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+function Example() {
+  return (
+    <>
+      <IonMenu type="push" contentId="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Auswahl</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          This is the menu content.
+        </IonContent>
+      </IonMenu>
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Start</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          Tap the button in the toolbar to open the menu.
+        </IonContent>
+      </IonPage>
+    </>
+  );
+}
+export default Example;
